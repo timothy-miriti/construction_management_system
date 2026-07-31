c1bb2a03456abf007652a065615f5f72f804510f
timothy-miriti <timothy.miriti@student.moringaschool.com>
2026-07-31 14:43:29 +0300

    update 1 of run file

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,13 @@
-from app import create_app
+# from app import create_app
 
-app = create_app('development')
+# app = create_app('development')
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
+
+
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
