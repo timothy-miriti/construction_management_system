92ab08020dbd620e0936a35039ce73da16f4e0f9
timothy-miriti <timothy.miriti@student.moringaschool.com>
2026-07-31 23:10:15 +0300

    fix the run file

--- backend/run.py
+++ backend/run.py
@@ -6,22 +6,26 @@
 #     app.run(debug=True, port=5000)
 
 
-# import os
-# from app import app  
-
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
-
 import os
 from app import create_app
 
-app = create_app("production")
+app = create_app(os.environ.get("FLASK_ENV", "development"))
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(
-        host="0.0.0.0",
-        port=port,
-        debug=False
-    )
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
+
+# import os
+# from app import create_app
+
+# app = create_app("production")
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(
+#         host="0.0.0.0",
+#         port=port,
+#         debug=False
+#     )
