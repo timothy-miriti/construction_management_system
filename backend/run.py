f4916277b4a6cca666d751245601a50a5fa5c142
timothy-miriti <timothy.miriti@student.moringaschool.com>
2026-07-31 21:24:09 +0300

    fix: update run.py for Render deployment

--- backend/run.py
+++ backend/run.py
@@ -6,9 +6,16 @@
 #     app.run(debug=True, port=5000)
 
 
-import os
-from app import app  
+# import os
+# from app import app  
+
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
+
+from app import create_app
+
+app = create_app('development')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, port=5000)
